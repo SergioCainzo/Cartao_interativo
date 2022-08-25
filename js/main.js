95f0fdfbd89971f471e7e24abd722f1789881678
Sergio Cainzo <97249973+SergioCainzo@users.noreply.github.com>
2022-08-25 17:00:42 -0300

    Implementação do DOM.
    Realizado a atualização do código para que seja feita a importação dos valores do input para os Cartões.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,44 @@
-let botao = document.querySelector('#button');
+// Variáveis dos seletores
+// Seletor Nome
+let cardName = document.querySelector('.card_name');
+let nome = document.querySelector('.name');
+
+// Seletor Código de Segurança
+let seguranca = document.querySelector('.sec');
+let cardSec = document.querySelector('.seguro');
+
+// Seletor número do cartão
+let numero = document.querySelector('.number');
+let cardNumber = document.querySelector('.card_number');
+
+// Seletor validade
+let mes = document.querySelector('.mm');
+let ano = document.querySelector('.yy');
+let cardValid = document.querySelector('.card_valid');
+
+
+// Const do click => botao
+const botao = document.querySelector('.button');
+
+
 
 botao.addEventListener('click', () => {
-    alert("Thank you!" + " We've added your card detail's" + " Continue.");
+    // Funções do cartão;
+    mudarCodigo();
+    
+    // Letras Maiúsculas
+    letrasMaiusculas();
 })
+
+
+function mudarCodigo(){
+    cardName.textContent = nome.value;
+    cardSec.textContent = seguranca.value;
+    cardNumber.textContent = numero.value;
+    cardValid.textContent = mes.value + '/' + ano.value;
+}
+
+
+function letrasMaiusculas(){
+    nome.value = nome.value.toUpperCase();
+}
